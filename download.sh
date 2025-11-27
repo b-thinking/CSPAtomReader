b425da1db47faeaed0bb15aedd41551ea460aea0
Jose M. Albarrán <jmalbarran@b-thinking.com>
2025-11-27 19:23:59 +0100

    Download Atom files scripts

---
+++ download.sh
@@ -0,0 +1,22 @@
+#!/bin/env bash
+
+# Download Atom file
+mkdir -p licitacionesPerfilesContratanteCompleto3
+wget -qO- "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_2020.zip"|tar -xvf- -C licitacionesPerfilesContratanteCompleto3
+wget -qO- "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_2021.zip"|tar -xvf- -C licitacionesPerfilesContratanteCompleto3
+wget -qO- "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_2022.zip"|tar -xvf- -C licitacionesPerfilesContratanteCompleto3
+wget -qO- "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_2023.zip"|tar -xvf- -C licitacionesPerfilesContratanteCompleto3
+wget -qO- "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_2024.zip"|tar -xvf- -C licitacionesPerfilesContratanteCompleto3
+wget -qO- "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_202501.zip"|tar -xvf- -C licitacionesPerfilesContratanteCompleto3
+wget -qO- "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_202502.zip"|tar -xvf- -C licitacionesPerfilesContratanteCompleto3
+wget -qO- "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_202503.zip"|tar -xvf- -C licitacionesPerfilesContratanteCompleto3
+wget -qO- "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_202504.zip"|tar -xvf- -C licitacionesPerfilesContratanteCompleto3
+wget -qO- "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_202505.zip"|tar -xvf- -C licitacionesPerfilesContratanteCompleto3
+wget -qO- "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_202506.zip"|tar -xvf- -C licitacionesPerfilesContratanteCompleto3
+wget -qO- "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_202507.zip"|tar -xvf- -C licitacionesPerfilesContratanteCompleto3
+wget -qO- "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_202508.zip"|tar -xvf- -C licitacionesPerfilesContratanteCompleto3
+wget -qO- "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_202509.zip"|tar -xvf- -C licitacionesPerfilesContratanteCompleto3
+wget -qO- "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_202510.zip"|tar -xvf- -C licitacionesPerfilesContratanteCompleto3
+wget -qO- "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_202511.zip"|tar -xvf- -C licitacionesPerfilesContratanteCompleto3
+
+
---
+++ download_linux.sh
@@ -0,0 +1,38 @@
+#!/bin/env bash
+
+# Download Atom file
+mkdir -p licitacionesPerfilesContratanteCompleto3
+wget "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_2020.zip"
+wget "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_2021.zip"
+wget "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_2022.zip"
+wget "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_2023.zip"
+wget "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_2024.zip"
+wget "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_202501.zip"
+wget "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_202502.zip"
+wget "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_202503.zip"
+wget "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_202504.zip"
+wget "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_202505.zip"
+wget "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_202506.zip"
+wget "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_202507.zip"
+wget "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_202508.zip"
+wget "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_202509.zip"
+wget "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_202510.zip"
+wget "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_202511.zip"
+
+
+unzip -o licitacionesPerfilesContratanteCompleto3_2020.zip -d licitacionesPerfilesContratanteCompleto3
+unzip -o licitacionesPerfilesContratanteCompleto3_2021.zip -d licitacionesPerfilesContratanteCompleto3
+unzip -o licitacionesPerfilesContratanteCompleto3_2022.zip -d licitacionesPerfilesContratanteCompleto3
+unzip -o licitacionesPerfilesContratanteCompleto3_2023.zip -d licitacionesPerfilesContratanteCompleto3
+unzip -o licitacionesPerfilesContratanteCompleto3_2024.zip -d licitacionesPerfilesContratanteCompleto3
+unzip -o licitacionesPerfilesContratanteCompleto3_202502.zip -d licitacionesPerfilesContratanteCompleto3
+unzip -o licitacionesPerfilesContratanteCompleto3_202503.zip -d licitacionesPerfilesContratanteCompleto3
+unzip -o licitacionesPerfilesContratanteCompleto3_202501.zip -d licitacionesPerfilesContratanteCompleto3
+unzip -o licitacionesPerfilesContratanteCompleto3_202504.zip -d licitacionesPerfilesContratanteCompleto3
+unzip -o licitacionesPerfilesContratanteCompleto3_202505.zip -d licitacionesPerfilesContratanteCompleto3
+unzip -o licitacionesPerfilesContratanteCompleto3_202506.zip -d licitacionesPerfilesContratanteCompleto3
+unzip -o licitacionesPerfilesContratanteCompleto3_202507.zip -d licitacionesPerfilesContratanteCompleto3
+unzip -o licitacionesPerfilesContratanteCompleto3_202508.zip -d licitacionesPerfilesContratanteCompleto3
+unzip -o licitacionesPerfilesContratanteCompleto3_202509.zip -d licitacionesPerfilesContratanteCompleto3
+unzip -o licitacionesPerfilesContratanteCompleto3_202510.zip -d licitacionesPerfilesContratanteCompleto3
+unzip -o licitacionesPerfilesContratanteCompleto3_202511.zip -d licitacionesPerfilesContratanteCompleto3
