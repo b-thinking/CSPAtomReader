f89e0bd56b550eb169c2e3219a8980da9265a337
Jose Manuel Albarrán <jmalbarran@b-thinking.com>
2026-04-07 11:42:12 +0200

    WIP: download script - changed to user system date and download all until now

--- download.sh
+++ download.sh
@@ -1,22 +1,26 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 
-# Download Atom file
+
+# Download Atom files
+# Years 2020-current_year-1: one file per year
+# Current year: one file per month
+
 mkdir -p licitacionesPerfilesContratanteCompleto3
-wget -qO- "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_2020.zip"|tar -xvf- -C licitacionesPerfilesContratanteCompleto3
-wget -qO- "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_2021.zip"|tar -xvf- -C licitacionesPerfilesContratanteCompleto3
-wget -qO- "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_2022.zip"|tar -xvf- -C licitacionesPerfilesContratanteCompleto3
-wget -qO- "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_2023.zip"|tar -xvf- -C licitacionesPerfilesContratanteCompleto3
-wget -qO- "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_2024.zip"|tar -xvf- -C licitacionesPerfilesContratanteCompleto3
-wget -qO- "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_202501.zip"|tar -xvf- -C licitacionesPerfilesContratanteCompleto3
-wget -qO- "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_202502.zip"|tar -xvf- -C licitacionesPerfilesContratanteCompleto3
-wget -qO- "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_202503.zip"|tar -xvf- -C licitacionesPerfilesContratanteCompleto3
-wget -qO- "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_202504.zip"|tar -xvf- -C licitacionesPerfilesContratanteCompleto3
-wget -qO- "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_202505.zip"|tar -xvf- -C licitacionesPerfilesContratanteCompleto3
-wget -qO- "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_202506.zip"|tar -xvf- -C licitacionesPerfilesContratanteCompleto3
-wget -qO- "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_202507.zip"|tar -xvf- -C licitacionesPerfilesContratanteCompleto3
-wget -qO- "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_202508.zip"|tar -xvf- -C licitacionesPerfilesContratanteCompleto3
-wget -qO- "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_202509.zip"|tar -xvf- -C licitacionesPerfilesContratanteCompleto3
-wget -qO- "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_202510.zip"|tar -xvf- -C licitacionesPerfilesContratanteCompleto3
-wget -qO- "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_202511.zip"|tar -xvf- -C licitacionesPerfilesContratanteCompleto3
 
+# Get current year and month from system date
+current_year=$(date +%Y)
+current_month=$(date +%m)
 
+# Download full years (2020 to previous year)
+for year in $(seq 2020 $(($current_year - 1))); do
+    echo "Downloading year $year"
+    wget -qO- "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_${year}.zip" | tar -xvf- -C licitacionesPerfilesContratanteCompleto3
+done
+
+# Download current year months (one file per month up to current month)
+for month in $(seq -w 1 $current_month); do
+    echo "Downloading year ${current_year} month ${month}"
+    wget -qO- "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_${current_year}${month}.zip" | tar -xvf- -C licitacionesPerfilesContratanteCompleto3
+done
+
+echo "Generated on $(date)" > licitacionesPerfilesContratanteCompleto3/README.txt
